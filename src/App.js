f087acb0fff518fa2b7931fa995cf79daaf7d5e5
Hemanth <hemanthhra12@gmail.com>
2025-03-29 06:49:43 -0300

    watchlist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MoviesGrid from './components/MoviesGrid'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
+import Watchlist from "./components/Watchlist";
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
@@ -12,7 +14,26 @@ function App() {
       <div className='container'> 
 
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path = "/" element={<MoviesGrid />}></Route>
+            <Route path = "/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+
+      
 
       </div>
       <Footer></Footer>
